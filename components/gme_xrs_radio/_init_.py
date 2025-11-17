81fbb4e1afa24b600469963bb6d93e72d8dfa4ba
andrewbackway <31836236+andrewbackway@users.noreply.github.com>
2025-11-17 18:09:19 +1100

    a

--- components/gme_xrs_radio/_init_.py
+++ components/gme_xrs_radio/_init_.py
@@ -4,34 +4,41 @@ import esphome.config_validation as cv
 from esphome.components import ble_client, text_sensor
 from esphome.const import CONF_ID
 
-DEPENDENCIES = ["ble_client"]
-AUTO_LOAD = ["text_sensor"]
-
 CONF_STATUS_TEXT = "status_text"
 
 gme_xrs_radio_ns = cg.esphome_ns.namespace("gme_xrs_radio")
+
 GmeXrsRadioComponent = gme_xrs_radio_ns.class_(
     "GmeXrsRadioComponent",
     cg.Component,
     ble_client.BLEClientNode,
 )
 
-CONFIG_SCHEMA = ble_client.BLE_CLIENT_SCHEMA.extend(
+CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(GmeXrsRadioComponent),
+
+        # Link to the existing ble_client:
+        cv.Required("ble_client_id"): cv.use_id(ble_client.BLEClient),
+
+        # Optional diagnostic text sensor – last raw line:
         cv.Optional(CONF_STATUS_TEXT): text_sensor.text_sensor_schema(
             icon="mdi:radio-tower",
             entity_category="diagnostic",
         ),
     }
-)
+).extend(cv.COMPONENT_SCHEMA)
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await ble_client.register_ble_node(var, config)
 
+    # Wire into the BLE client
+    ble_parent = await cg.get_variable(config["ble_client_id"])
+    cg.add(ble_parent.register_ble_node(var))
+
+    # Optional status text sensor
     if CONF_STATUS_TEXT in config:
         ts = await text_sensor.new_text_sensor(config[CONF_STATUS_TEXT])
         cg.add(var.set_status_text_sensor(ts))
