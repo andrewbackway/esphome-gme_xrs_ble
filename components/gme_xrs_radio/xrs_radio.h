cebcb8018394c8c1dad5b3d347e29cf4c9db7ea2
andrewbackway <31836236+andrewbackway@users.noreply.github.com>
2025-11-18 17:16:27 +1100

    #

--- components/gme_xrs_radio/xrs_radio.h
+++ components/gme_xrs_radio/xrs_radio.h
@@ -148,17 +148,9 @@ class XRSRadioComponent : public Component,
   void set_location_interval(uint32_t interval_ms) {
     location_interval_ms_ = interval_ms;
   }
-  void set_latitude_sensor(sensor::Sensor* sensor) {
-    latitude_sensor_ = sensor;
-  }
-  void set_longitude_sensor(sensor::Sensor* sensor) {
-    longitude_sensor_ = sensor;
-  }
-  void set_location_interval(uint32_t interval_ms) {
-    location_interval_ms_ = interval_ms;
-  }
-
-  //optional message source (text sensor) for WGTMSG payload
+  // manual "send location + message now" for the button
+  void send_location_with_message();
+  // optional message source (text sensor) for WGTMSG payload
   void set_message_sensor(text_sensor::TextSensor* sensor) {
     message_sensor_ = sensor;
   }
